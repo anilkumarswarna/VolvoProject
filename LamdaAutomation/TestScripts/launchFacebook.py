4b645ecad3f14e7de036801972b8d31ace52c719
anilkumarswarna <anilkumarswarna158@gmail.com>
2023-04-09 18:09:04 +0530

    Browser Project added

---
+++ LamdaAutomation/TestScripts/launchFacebook.py
@@ -0,0 +1,11 @@
+from Utils.browserLaunch import browserLaunch
+import time
+
+
+obj = browserLaunch()
+obj.launchEdge()
+obj.openUrl()
+obj.maxWindow()
+obj.class_Name_Val1()
+obj.exitAllWindows()
+
