d19d1151482b804918ac8c8f4fd4cb7962a80361
anilkumarswarna <128887265+anilkumarswarna@users.noreply.github.com>
2023-04-22 14:28:41 +0530

    Delete LamdaAutomation directory

--- LamdaAutomation/TestScripts/Contact/launchContact.py
+++
@@ -1,6 +0,0 @@
-from LamdaAutomation.Utils.Contacts import contactLibrary
-
-obj1 = contactLibrary.contactLibrary()
-
-obj1.launchContact()
-obj1.pressBack()
--- LamdaAutomation/TestScripts/Contact/launchContactUi.py
+++
@@ -1,9 +0,0 @@
-from LamdaAutomation.Utils.Contacts import contactLibraryUi
-import time
-
-obj = contactLibraryUi.contactLibraryUi()
-
-obj.launchContactUi()
-time.sleep(2)
-obj.addcontact()
-obj.inputfirstname()
